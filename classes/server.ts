4f5c06e0252a9dca1e93d286fd499406d3c47dda
Max Alva <alva85@gmail.com>
2020-03-01 18:12:13 -0500

    Configuracion de sockets

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,31 @@
 import express from 'express';
+import socketIO from 'socket.io'
+import http from 'http';
 
 export default class Server {
 
 	public app: express.Application;
 	public port: number;
+	public io: socketIO.Server;
+	private httpServer: http.Server;
 
 	constructor() {
 		this.app = express();
 		this.port = Number(process.env.SERVER_PORT || 3000);
+
+		this.httpServer = new http.Server(this.app);
+
+		this.io = socketIO(this.httpServer);
 	}
 
 	start(callback: () => void) {
-		this.app.listen(this.port, callback);
+		this.httpServer.listen(this.port, callback);
+	}
+
+	private listenSockets() {
+		this.io.on('connection', client => {
+			console.log('Cliente conectado');
+		})
 	}
 
 }
