a0998dc101367096d4126b0aa821ad43e29a59c3
Max Alva <alva85@gmail.com>
2020-03-01 20:33:38 -0500

    emit events

--- classes/server.ts
+++ classes/server.ts
@@ -1,21 +1,29 @@
 import express from 'express';
 import socketIO from 'socket.io'
 import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server {
 
+	private static _instance: Server;
 	public app: express.Application;
 	public port: number;
 	public io: socketIO.Server;
-	private httpServer: http.Server;
+	private readonly httpServer: http.Server;
 
-	constructor() {
+	private constructor() {
 		this.app = express();
 		this.port = Number(process.env.SERVER_PORT || 3000);
 
 		this.httpServer = new http.Server(this.app);
 
 		this.io = socketIO(this.httpServer);
+
+		this.listenSockets();
+	}
+
+	public static get instance() {
+		return this._instance || (this._instance = new this());
 	}
 
 	start(callback: () => void) {
@@ -25,6 +33,9 @@ export default class Server {
 	private listenSockets() {
 		this.io.on('connection', client => {
 			console.log('Cliente conectado');
+
+			socket.message(client, this.io);
+			socket.disconnect(client);
 		})
 	}
 
---
+++ sockets/sockets.ts
@@ -0,0 +1,16 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io'
+
+export const disconnect = ( client: Socket ) => {
+	client.on( 'disconnect', () => {
+		console.log( 'Cliente desconectado' );
+	} );
+};
+
+export const message = ( client: Socket, io: socketIO.Server ) => {
+	client.on('message', (payload: any) => {
+		console.log(payload);
+
+		io.emit('new-message', payload);
+	})
+};
